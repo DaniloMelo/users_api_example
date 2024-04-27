20867840c3b21da3ab02ff3418baa470d2ebb07d
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-04-26 22:20:58 -0300

    feat: create readByIdController.js and readByIdService.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,12 @@
 import express from "express"
 import readAllController from "../controllers/readAllController.js"
+import readByIdController from "../controllers/readByIdController.js"
 import createUserController from "../controllers/createUserController.js"
 
 const router = express.Router()
 
 router.get("/users", readAllController)
+router.get("/users/:id", readByIdController)
 router.post("/users", createUserController)
 
 export default router
