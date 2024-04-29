ee962da838af11d7d049fcea6d47b8ff5d8dc412
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-04-29 13:32:27 -0300

    fix: partialUpdateUserController.js now only updates existing users

--- src/controllers/partialUpdateUserController.js
+++ src/controllers/partialUpdateUserController.js
@@ -1,30 +1,26 @@
 import partialUpdateUserService from "../services/partialUpdateUserService.js";
 import readUserByIdService from "../services/readUserByIdService.js"
 
-export default async function (request, response) {
+export default async function partialUpdateUserController(request, response) {
   try {
     const { id } = request.params
     const { userName, userEmail } = request.body
 
-    // if (!userName || !userEmail) {
-    //   response.status(400).json({ error: "User name or Email not provided." })
-    //   throw new Error("User name or Email not provided.")
-    // }
+    const [result] = await readUserByIdService(id)
 
-    if (!id || isNaN(id)) {
-      response.status(400).json({ error: "User ID not provided or invalid." })
-      throw new Error("User ID not provided or invalid. ")
+    if (result.length === 0 || result === null || result === undefined) {
+      return response.status(404).json({ error: "User not found." })
     }
 
-    let bodyValues = { name: userName, email: userEmail }
+    const bodyValues = { name: userName, email: userEmail }
 
     await partialUpdateUserService(id, bodyValues)
 
-    const [result] = await readUserByIdService(id)
+    const [updatedResult] = await readUserByIdService(id)
 
     response.status(200).json({
       message: "User partially updated successfully.",
-      data: result
+      data: updatedResult
     })
 
   } catch (error) {
